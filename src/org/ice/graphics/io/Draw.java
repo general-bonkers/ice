5a50319d1f7f9517d161b78dc4e659b7a8bde425
adam <akewley_128@hotmail.com>
2012-12-03 18:12:45 -0500

    updating draw. Adding 8 bit dos palette

--- src/org/ice/graphics/io/Draw.java
+++ src/org/ice/graphics/io/Draw.java
@@ -6,6 +6,12 @@ import java.awt.image.BufferStrategy;
 
 public class Draw {
 
+	
+	private static Color getColor( int color )
+	{
+		return DOSPalette.pal[ color ];
+	}
+	
 	private java.awt.Graphics graphics; 
 	public Draw( BufferStrategy bufferStrategy )
 	{
@@ -24,20 +30,21 @@ public class Draw {
 	}
 	public void cls ( int color )
 	{
-		graphics.setColor( Color.blue );
+		graphics.setColor( getColor( color ) );
 		//!HACK get resolution from parent object?
 		graphics.fillRect( 0, 0, 640, 480 );
 	}
 	
 	public void line( int x, int y, int x2, int y2, int color )
 	{
-		graphics.setColor( Color.MAGENTA );
+		graphics.setColor( getColor( color ) );
+		
 		graphics.drawLine( x, y, x2, y2 );
 	}
 	
 	public void box( int x, int y, int w, int h, int color, boolean bf )
 	{
-		graphics.setColor( new Color(255, 255, 0 ) );
+		graphics.setColor( getColor( color ) );
 		if ( bf )
 		{
 			graphics.fillRect( x, y, w, h );
@@ -50,7 +57,7 @@ public class Draw {
 	
 	public void pset( int x, int y, int color )
 	{
-		graphics.setColor( new Color( 255, 255, 0 ) );
+		graphics.setColor( getColor( color ) );
 		graphics.drawLine( x, y, x, y );
 	}
 	
